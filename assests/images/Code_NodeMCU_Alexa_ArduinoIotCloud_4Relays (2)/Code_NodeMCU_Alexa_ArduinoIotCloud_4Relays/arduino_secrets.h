09c9a6ff1521c7ea30365e6cc0bb883d8c6275ac
vineethkv7736 <vineethkv7736@gmail.com>
2023-09-24 21:07:57 +0530

    website 50% done

--- assests/images/Code_NodeMCU_Alexa_ArduinoIotCloud_4Relays (2)/Code_NodeMCU_Alexa_ArduinoIotCloud_4Relays/arduino_secrets.h
+++
@@ -1,3 +0,0 @@
-#define SECRET_SSID "Asianet-896DA8"  //Enter WIFI NAME
-#define SECRET_PASS "fancycarrot@lab1"  //Enter WIFI PASSWORD
-#define SECRET_DEVICE_KEY "NUZVATO7PVL9X7JANZGO"  //Enter Secret Key
